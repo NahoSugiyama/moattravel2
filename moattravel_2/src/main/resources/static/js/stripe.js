ec99185c8d172742a261dda791cf3fc67eebb835
NahoSugiyama <sgymaspk@gmail.com>
2025-02-06 16:53:09 +0900

    Delete moattravel_2 directory
    やり直し

--- moattravel_2/src/main/resources/static/js/stripe.js
+++
@@ -1,8 +0,0 @@
-const stripe = Stripe('pk_test_51QliOXCIzQesVfMVXxQOYua8cJJOfHuYcoXDk9Mjel17g7MD93mWqYqWdfFkbsgOyjmh51dKnojm7k4XW2CxJEwH00dvKdoUn0');
-const paymentButton = document.querySelector('#paymentButton');
-
-paymentButton.addEventListener('click', () => {
-	stripe.redirectToCheckout({
-		sessionId: sessionId		
-	})
-});
